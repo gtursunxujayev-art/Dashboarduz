4f7b55012d87403bf0fcf8606c295bc0e0e45539
gtursunxujayev-art <gtursunxujayev@gmail.com>
2026-01-15 15:42:22 +0500

    Create variables.tf

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "dashboarduz"
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_username" {
+  description = "RDS master username"
+  type        = string
+  default     = "dashboarduz"
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "RDS master password"
+  type        = string
+  sensitive   = true
+}
+
+variable "redis_node_type" {
+  description = "ElastiCache Redis node type"
+  type        = string
+  default     = "cache.t3.micro"
+}
